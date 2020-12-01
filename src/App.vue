f970243ba9afe8d08c106095485cdc94ba6300d6
sapphi-red <green@sapphi.red>
2020-12-01 17:19:51 +0900

    Layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
+  <page-header />
   <router-view />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import PageHeader from '/@/components/Layout/PageHeader.vue'
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  components: {
+    PageHeader
+  }
 })
 </script>
---
+++ src/components/Layout/PageHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :class="$style.container">
+    <div>Logo</div>
+    <div>search</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+
+export default defineComponent({
+  name: 'Header',
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss" module>
+.container {
+  display: flex;
+}
+</style>
