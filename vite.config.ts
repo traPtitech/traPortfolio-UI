3d6a4bc5946cd8e13f4f08c5de00d9bc539713b3
sapphi-red <green@sapphi.red>
2020-12-01 16:55:26 +0900

    Add color palette

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,11 @@ const config: UserConfig = {
       changeOrigin: true
     }
   },
+  cssPreprocessOptions: {
+    scss: {
+      additionalData: `@import "${srcPath}/styles/common.scss";`
+    }
+  },
   plugins: [PurgeIcons()]
 }
 
