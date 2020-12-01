f0e4bcf01a325cfbb369ded8f78cf9008d89746e
sapphi-red <green@sapphi.red>
2020-12-01 16:53:13 +0900

    Add Icon.vue

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import router from './router'
 import store from './store'
 import App from './App.vue'
+
 import './index.scss'
+import '@purge-icons/generated'
 
 const app = createApp(App)
 app.use(router)
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
 import { UserConfig } from 'vite'
 import path from 'path'
+import PurgeIcons from 'vite-plugin-purge-icons'
+
+const srcPath = path.resolve(__dirname, 'src').replace(/\\/g, '/')
 
 const config: UserConfig = {
   alias: {
-    '/@/': path.resolve(__dirname, 'src')
+    '/@/': srcPath
   },
   proxy: {
     '/api': {
       target: 'http://localhost:4010',
       changeOrigin: true
     }
-  }
+  },
+  plugins: [PurgeIcons()]
 }
 
 export default config
