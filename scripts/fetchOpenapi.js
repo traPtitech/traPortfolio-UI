1be5cfa9f6d4ace24af0dd1dafeeb2976d65a874
sapphi-red <green@sapphi.red>
2020-12-05 21:58:41 +0900

    setup CI close #1

--- scripts/fetchOpenapi.js
+++ scripts/fetchOpenapi.js
@@ -8,6 +8,12 @@ const URL =
   'https://raw.githubusercontent.com/traPtitech/traPortfolio/master/docs/swagger/traPortfolio.v1.yaml'
 const dist = './traPortfolio.v1.yaml'
 
+if (process.env.SKIP_GENAPI) {
+  // eslint-disable-next-line no-console
+  console.log('Skipped fetching openapi.')
+  return
+}
+
 ;(async () => {
   const res = await fetch(URL)
   const body = await res.text()
