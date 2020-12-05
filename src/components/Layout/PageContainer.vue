9f47b452e7f992ab91d995caa4dab55e13788672
sapphi-red <green@sapphi.red>
2020-12-05 18:11:51 +0900

    groupsの実装

---
+++ src/components/Layout/PageContainer.vue
@@ -0,0 +1,18 @@
+<template>
+  <main :class="$style.container"><slot /></main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageContainer'
+})
+</script>
+
+<style lang="scss" module>
+.container {
+  max-width: 1024px;
+  margin: 0 auto;
+}
+</style>
--- src/pages/Groups.vue
+++ src/pages/Groups.vue
@@ -1,18 +1,26 @@
 <template>
-  <div>Groups</div>
-  <ul v-if="fetcherState === 'loaded' && groups">
-    <li v-for="group in groups" :key="group.id">{{ group.name }}</li>
-  </ul>
-  <p v-else>{{ fetcherState }}</p>
+  <page-container>
+    <page-title :class="$style.title">班一覧</page-title>
+    <group-list v-if="fetcherState === 'loaded'" :groups="groups" />
+    <p v-else>{{ fetcherState }}</p>
+  </page-container>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue'
+import { defineComponent, computed } from 'vue'
 import { useStore } from '/@/store'
 import useFetcher from '/@/use/fetcher'
+import PageContainer from '/@/components/Layout/PageContainer.vue'
+import PageTitle from '/@/components/Layout/PageTitle.vue'
+import GroupList from '/@/components/Groups/GroupList.vue'
 
 export default defineComponent({
   name: 'Groups',
+  components: {
+    PageContainer,
+    PageTitle,
+    GroupList
+  },
   setup() {
     const store = useStore()
 
@@ -25,3 +33,9 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="scss" module>
+.title {
+  margin: 4rem 0;
+}
+</style>
