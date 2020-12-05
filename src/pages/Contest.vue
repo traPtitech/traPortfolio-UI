23a781edd084429ae5b3163aab798d9bec5123da
sapphi-red <green@sapphi.red>
2020-12-05 22:07:34 +0900

    fix build

---
+++ src/pages/Contest.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Contest</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Contest'
+})
+</script>
---
+++ src/pages/ContestTeam.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>ContestTeam</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ContestTeam'
+})
+</script>
