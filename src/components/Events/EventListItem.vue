e8ed6acb27dbbba0ca628f930c2a150f05b0b05a
toshi/pono000 <toshi.pono.h@gmail.com>
2020-12-17 23:18:28 +0900

    eventsに日付の追加

--- src/components/Events/EventListItem.vue
+++ src/components/Events/EventListItem.vue
@@ -2,13 +2,15 @@
   <div>
     <router-link :class="$style.link" :to="`/events/${event.id}`">
       <span :class="$style.name">{{ event.name }}</span>
+      <span :class="$style.date">({{ state.date }})</span>
     </router-link>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { computed, defineComponent, PropType, reactive } from 'vue'
 import { Event } from '/@/lib/apis'
+import { getSinceDate } from '/@/lib/date'
 
 export default defineComponent({
   name: 'EventListItem',
@@ -17,6 +19,12 @@ export default defineComponent({
       type: Object as PropType<Event>,
       required: true
     }
+  },
+  setup(props) {
+    const state = reactive({
+      date: computed(() => getSinceDate(props.event.duration.since))
+    })
+    return { state }
   }
 })
 </script>
@@ -28,5 +36,9 @@ export default defineComponent({
 .name {
   color: $color-text;
   font-size: 3rem;
+}
+.date {
+  color: $color-text;
+  font-size: 1.5rem;
 }
 </style>
