8cb87527f033f9e5fc09e4a54db05c2f72948321
toshi/pono000 <toshi.pono.h@gmail.com>
2020-12-17 22:20:24 +0900

    eventsの実装

---
+++ src/components/Events/EventListItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <router-link :class="$style.link" :to="`/events/${event.id}`">
+      <span :class="$style.name">{{ event.name }}</span>
+    </router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { Event } from '/@/lib/apis'
+
+export default defineComponent({
+  name: 'EventListItem',
+  props: {
+    event: {
+      type: Object as PropType<Event>,
+      required: true
+    }
+  }
+})
+</script>
+
+<style lang="scss" module>
+.link {
+  text-decoration: none;
+}
+.name {
+  color: $color-text;
+  font-size: 3rem;
+}
+</style>
--- src/pages/Events.vue
+++ src/pages/Events.vue
@@ -1,11 +1,41 @@
 <template>
-  <div>Events</div>
+  <page-container>
+    <page-title :class="$style.title">イベント一覧</page-title>
+    <event-list v-if="fetcherState === 'loaded'" :events="events" />
+    <p v-else>{{ fetcherState }}</p>
+  </page-container>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, computed } from 'vue'
+import { useStore } from '/@/store'
+import useFetcher from '/@/use/fetcher'
+import PageContainer from '/@/components/Layout/PageContainer.vue'
+import PageTitle from '/@/components/Layout/PageTitle.vue'
+import EventList from '/@/components/Events/EventList.vue'
 
 export default defineComponent({
-  name: 'Events'
+  name: 'Events',
+  components: {
+    PageContainer,
+    PageTitle,
+    EventList
+  },
+  setup() {
+    const store = useStore()
+
+    const events = computed(() => store.state.events)
+    const { fetcherState } = useFetcher(events, () =>
+      store.dispatch.fetchEvents()
+    )
+
+    return { events, fetcherState }
+  }
 })
 </script>
+
+<style lang="scss" module>
+.title {
+  margin: 4rem 0;
+}
+</style>
