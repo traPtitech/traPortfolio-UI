72ecbf1828e2b17ed458023613299a9edac19ca5
tesso <ta4780@icloud.com>
2020-12-23 15:22:26 +0900

    projectsを実装

---
+++ src/components/Projects/ProjectListItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <router-link :class="$style.link" :to="`/projects/${project.id}`">
+      <span :class="$style.name">{{ project.name }}</span>
+    </router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { Project } from '/@/lib/apis'
+
+export default defineComponent({
+  name: 'ProjectListItem',
+  props: {
+    project: {
+      type: Object as PropType<Project>,
+      required: true
+    }
+  }
+})
+</script>
+
+<style lang="scss" module>
+.link {
+  text-decoration: none;
+}
+.name {
+  color: $color-text;
+  font-size: 3rem;
+}
+</style>
