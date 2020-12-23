2a1f9f1f75b0043daede7110a3ada69079c14a54
tesso <ta4780@icloud.com>
2020-12-23 15:10:43 +0900

    Projectsをstoreに追加

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,6 +1,6 @@
 import { defineActions } from 'direct-vuex'
 import { rootActionContext } from '.'
-import apis, { Group } from '/@/lib/apis'
+import apis, { Group, Project } from '/@/lib/apis'
 
 export const actions = defineActions({
   async fetchGroups(context): Promise<Group[]> {
@@ -12,6 +12,17 @@ export const actions = defineActions({
 
     const res = await apis.getGroups()
     commit.setGroups(res.data)
+    return res.data
+  },
+  async fetchProjects(context): Promise<Project[]> {
+    const { state, commit } = rootActionContext(context)
+
+    if (state.projects !== null) {
+      return state.projects
+    }
+
+    const res = await apis.getProjects()
+    commit.setProjects(res.data)
     return res.data
   }
 })
--- src/store/mutations.ts
+++ src/store/mutations.ts
@@ -1,9 +1,12 @@
 import { defineMutations } from 'direct-vuex'
 import { S } from './state'
-import { Group } from '/@/lib/apis'
+import { Group, Project } from '/@/lib/apis'
 
 export const mutations = defineMutations<S>()({
   setGroups(state: S, groups: Group[]) {
     state.groups = groups
+  },
+  setProjects(state:S,projects: Project[]){
+    state.projects = projects;
   }
 })
