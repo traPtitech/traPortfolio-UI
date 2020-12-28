f9942305128b442ac03fb12373ed25c00dad80e9
toshi/pono000 <toshi.pono.h@gmail.com>
2020-12-28 16:38:06 +0900

    hostnameListの作成

---
+++ src/components/Event/HostnameList.vue
@@ -0,0 +1,36 @@
+<template>
+  <section :class="$style.container">
+    <hostname-list-item
+      v-for="hostname in hostnames"
+      :key="hostname.id"
+      :hostname="hostname"
+    />
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { User } from '/@/lib/apis'
+import HostnameListItem from './HostnameListItem.vue'
+
+export default defineComponent({
+  name: 'HostnameList',
+  components: {
+    HostnameListItem
+  },
+  props: {
+    hostnames: {
+      type: Array as PropType<User[]>,
+      default: []
+    }
+  }
+})
+</script>
+
+<style lang="scss" module>
+.container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(30rem, 1fr));
+  gap: 2rem;
+}
+</style>
