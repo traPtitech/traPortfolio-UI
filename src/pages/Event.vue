7201cbda10177de45a722dcb0cdd6412d13c3be7
toshi/pono000 <toshi.pono.h@gmail.com>
2020-12-28 15:36:03 +0900

    Event:ヘッダー部分の作成

--- src/pages/Event.vue
+++ src/pages/Event.vue
@@ -1,11 +1,54 @@
 <template>
-  <div>Event</div>
+  <page-container>
+    <div :class="$style.titleContainer">
+      <page-title>{{ name }}</page-title>
+      <external-link :href="link" :class="$style.link">
+        説明ページ
+      </external-link>
+    </div>
+  </page-container>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, computed } from 'vue'
+import { useStore } from '/@/store'
+import useFetcher from '/@/use/fetcher'
+import PageContainer from '/@/components/Layout/PageContainer.vue'
+import PageTitle from '/@/components/Layout/PageTitle.vue'
+import ExternalLink from '/@/components/UI/ExternalLink.vue'
+import useParam from '/@/use/param'
 
 export default defineComponent({
-  name: 'Event'
+  name: 'Event',
+  components: {
+    PageContainer,
+    PageTitle,
+    ExternalLink
+  },
+  setup() {
+    const store = useStore()
+    const eventId = useParam('eventId')
+
+    const eventDetail = computed(() => store.state.eventDetail[eventId.value])
+    const { fetcherState } = useFetcher(eventDetail, () =>
+      store.dispatch.fetchEventDetail(eventId.value)
+    )
+
+    const name = computed(
+      () => eventDetail.value?.name ?? 'Loading... イベント'
+    )
+    const link = 'https://trap.jp' // TODO
+
+    return { fetcherState, name, link }
+  }
 })
 </script>
+
+<style lang="scss" module>
+.titleContainer {
+  margin: 4rem 0;
+}
+.link {
+  color: $color-secondary-text;
+}
+</style>
