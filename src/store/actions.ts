6924890dccb4a3c5d1d0e38dec902f853a93efd8
toshi/pono000 <toshi.pono.h@gmail.com>
2020-12-28 15:34:59 +0900

    storeにeventDetailを追加

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,6 +1,6 @@
 import { defineActions } from 'direct-vuex'
 import { rootActionContext } from '.'
-import apis, { Group, Event } from '/@/lib/apis'
+import apis, { Group, Event, EventDetail } from '/@/lib/apis'
 
 export const actions = defineActions({
   async fetchGroups(context): Promise<Group[]> {
@@ -23,6 +23,26 @@ export const actions = defineActions({
 
     const res = await apis.getEvents()
     commit.setEvents(res.data)
+    return res.data
+  },
+  async fetchEventDetail(
+    context,
+    eventId: string
+  ): Promise<EventDetail | undefined> {
+    const { state, commit } = rootActionContext(context)
+
+    if (state.eventDetail[eventId] !== undefined) {
+      return state.eventDetail[eventId]
+    }
+
+    const res = await apis.getEvent(eventId)
+
+    // 今はapiをたたくたびに値が変わる（event uuid もランダム）のでeventIdを一致させておく
+    // nameなどもランダムなので，EventsページとEventページの表示が異なる
+    res.data.id = eventId
+    // *******************
+
+    commit.setEventDetail(res.data)
     return res.data
   }
 })
--- src/store/state.ts
+++ src/store/state.ts
@@ -1,11 +1,13 @@
-import { Group, Event } from '/@/lib/apis'
+import { Group, Event, EventDetail } from '/@/lib/apis'
 
 export interface S {
   groups: Group[] | null
   events: Event[] | null
+  eventDetail: Record<string, EventDetail | undefined>
 }
 
 export const state: S = {
   groups: null,
-  events: null
+  events: null,
+  eventDetail: {}
 }
