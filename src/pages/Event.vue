951c7666afa766c119dd4852794b542142e2330f
toshi/pono000 <toshi.pono.h@gmail.com>
2020-12-28 17:07:25 +0900

    getEventを直接たたくようにする

--- src/pages/Event.vue
+++ src/pages/Event.vue
@@ -5,43 +5,43 @@
       <external-link :href="link" :class="$style.link">
         説明ページ
       </external-link>
-      <hostname-list v-if="fetcherState === 'loaded'" :hostnames="hostnames" />
-      <p v-else>{{ fetcherState }}</p>
+      <hostname-list :hostnames="hostnames" />
     </div>
   </page-container>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from 'vue'
-import { useStore } from '/@/store'
-import useFetcher from '/@/use/fetcher'
+import { defineComponent, computed, ref } from 'vue'
 import PageContainer from '/@/components/Layout/PageContainer.vue'
 import PageTitle from '/@/components/Layout/PageTitle.vue'
 import ExternalLink from '/@/components/UI/ExternalLink.vue'
 import useParam from '/@/use/param'
+import HostnameList from '../components/Event/HostnameList.vue'
+import apis, { EventDetail } from '/@/lib/apis'
+
+const getEvent = async (eventId: string): Promise<EventDetail> =>
+  (await apis.getEvent(eventId)).data
 
 export default defineComponent({
   name: 'Event',
   components: {
     PageContainer,
     PageTitle,
-    ExternalLink
+    ExternalLink,
+    HostnameList
   },
   setup() {
-    const store = useStore()
     const eventId = useParam('eventId')
-
-    const eventDetail = computed(() => store.state.eventDetail[eventId.value])
-    const { fetcherState } = useFetcher(eventDetail, () =>
-      store.dispatch.fetchEventDetail(eventId.value)
-    )
+    const eventDetail = ref<EventDetail>()
+    getEvent(eventId.value).then(response => (eventDetail.value = response))
 
     const name = computed(
       () => eventDetail.value?.name ?? 'Loading... イベント'
     )
     const link = 'https://trap.jp' // TODO
+    const hostnames = computed(() => eventDetail.value?.hostname)
 
-    return { fetcherState, name, link }
+    return { name, link, hostnames }
   }
 })
 </script>
