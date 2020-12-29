70c6810dfa68b5c22e5225ef87dd48d465544c8f
tesso <ta4780@icloud.com>
2020-12-29 15:18:31 +0900

    タイトルを設置

--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -1,11 +1,52 @@
 <template>
-  <div>Project</div>
+  <page-container>
+    <div :class="$style.titleContainer">
+      <page-title>{{ name }}</page-title>
+      <external-link :href="link" :class="$style.link">
+        紹介ページ
+      </external-link>
+    </div>
+  </page-container>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, ref, defineComponent, watchEffect } from 'vue'
+import PageContainer from '/@/components/Layout/PageContainer.vue'
+import PageTitle from '/@/components/Layout/PageTitle.vue'
+import ExternalLink from '/@/components/UI/ExternalLink.vue'
+import useParam from '/@/use/param'
+import apis, { ProjectDetail } from '/@/lib/apis'
+
+const getProject = async (projectId: string): Promise<ProjectDetail> =>
+  (await apis.getProject(projectId)).data
 
 export default defineComponent({
-  name: 'Project'
+  name: 'Project',
+  components: { PageContainer, PageTitle, ExternalLink },
+  setup() {
+    const projectId = useParam('projectId')
+    const projectDetail = ref<ProjectDetail>()
+    watchEffect(() => {
+      getProject(projectId.value).then(
+        response => (projectDetail.value = response)
+      )
+    })
+
+    const name = computed(
+      () => projectDetail.value?.name ?? 'Loading... プロジェクト'
+    )
+
+    const link = 'https://trap.jp'
+    return { name, link }
+  }
 })
 </script>
+
+<style lang="scss" module>
+.titleContainer {
+  margin: 4rem 0;
+}
+.link {
+  color: $color-secondary-text;
+}
+</style>
