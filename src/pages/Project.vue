326087a64f73e457a1d698c62e606d3fd779386e
tesso <ta4780@icloud.com>
2020-12-29 15:50:53 +0900

    メンバー一覧を表示

--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -6,6 +6,7 @@
         紹介ページ
       </external-link>
     </div>
+    <member-list v-if="members !== undefined" :members="members" />
   </page-container>
 </template>
 
@@ -16,13 +17,19 @@ import PageTitle from '/@/components/Layout/PageTitle.vue'
 import ExternalLink from '/@/components/UI/ExternalLink.vue'
 import useParam from '/@/use/param'
 import apis, { ProjectDetail } from '/@/lib/apis'
+import MemberList from '/@/components/Project/MemberList.vue'
 
 const getProject = async (projectId: string): Promise<ProjectDetail> =>
   (await apis.getProject(projectId)).data
 
 export default defineComponent({
   name: 'Project',
-  components: { PageContainer, PageTitle, ExternalLink },
+  components: {
+    PageContainer,
+    PageTitle,
+    ExternalLink,
+    MemberList
+  },
   setup() {
     const projectId = useParam('projectId')
     const projectDetail = ref<ProjectDetail>()
@@ -35,9 +42,9 @@ export default defineComponent({
     const name = computed(
       () => projectDetail.value?.name ?? 'Loading... プロジェクト'
     )
-
     const link = 'https://trap.jp'
-    return { name, link }
+    const members = computed(() => projectDetail.value?.members)
+    return { name, link, members }
   }
 })
 </script>
