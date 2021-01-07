04d7ad5251cc43b2ff27e092dd0dfb65f8735cb6
sapphi-red <green@sapphi.red>
2021-01-08 00:23:01 +0900

    Update vite to v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,32 @@
 import { UserConfig } from 'vite'
 import path from 'path'
+import VuePlugin from '@vitejs/plugin-vue'
 import PurgeIcons from 'vite-plugin-purge-icons'
 
 const srcPath = path.resolve(__dirname, 'src').replace(/\\/g, '/')
 
 const config: UserConfig = {
   alias: {
-    '/@/': srcPath
+    '/@': srcPath
   },
-  proxy: {
-    '/api': {
-      target: 'http://localhost:4010',
-      rewrite: path =>
-        path.startsWith('/api/v1') ? path.slice('/api/v1'.length) : path,
-      changeOrigin: true
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4010',
+        rewrite: path =>
+          path.startsWith('/api/v1') ? path.slice('/api/v1'.length) : path,
+        changeOrigin: true
+      }
     }
   },
-  cssPreprocessOptions: {
-    scss: {
-      additionalData: `@import "${srcPath}/styles/common.scss";`
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "${srcPath}/styles/common.scss";`
+      }
     }
   },
-  plugins: [PurgeIcons()]
+  plugins: [VuePlugin(), PurgeIcons()]
 }
 
 export default config
