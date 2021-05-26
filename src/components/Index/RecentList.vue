250d61dc0cc32af3709a4b3edd91170a3904d4a3
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-05-26 18:33:39 +0900

    Add 最近の実績（コンテスト）を追加

---
+++ src/components/Index/RecentList.vue
@@ -0,0 +1,72 @@
+<template>
+  <section>
+    <section-title :class="$style.title">
+      <span :class="$style.prefixTitle">最近の</span>
+      <span>{{ title }}</span>
+    </section-title>
+    <div>
+      <router-link
+        v-for="item in items.slice(0, 5)"
+        :key="item.id"
+        :to="`/${path}/${item.id}`"
+        :class="$style.link"
+      >
+        <span :class="$style.name">{{ item.name }}</span>
+      </router-link>
+    </div>
+    <router-link :to="`/${path}`" :class="$style.moreRead">
+      <span :class="$style.name">もっと見る</span>
+    </router-link>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { Contest, Project, Event } from '/@/lib/apis'
+import SectionTitle from '../Layout/SectionTitle.vue'
+
+export default defineComponent({
+  name: 'RecentList',
+  components: {
+    SectionTitle
+  },
+  props: {
+    items: {
+      type: Array as PropType<Contest[] | Project[] | Event[]>,
+      default: []
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    path: {
+      type: String,
+      default: ''
+    }
+  }
+})
+</script>
+
+<style lang="scss" module>
+.link {
+  display: block;
+  margin: 4px 1rem;
+  text-decoration: none;
+}
+.name {
+  font-size: 1.5rem;
+  color: $color-text;
+}
+.title {
+  color: $color-primary;
+}
+.prefix-title {
+  font-size: 1.15rem;
+}
+.more-read {
+  display: block;
+  margin: 0.5rem 1rem;
+  text-decoration: none;
+  text-align: right;
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,16 @@
 <template>
   <page-container>
     <group-name-list v-if="fetcherStateGroups" :groups="groups" />
+    <p v-else>{{ fetcherStateGroups }}</p>
+    <div :class="$style.container">
+      <recent-list
+        v-if="fetcherStateContests"
+        :items="contests"
+        title="実績"
+        path="contests"
+      />
+      <p v-else>{{ fetcherStateContests }}</p>
+    </div>
   </page-container>
 </template>
 
@@ -9,13 +19,15 @@ import { computed, defineComponent } from 'vue'
 import { useStore } from '/@/store'
 import useFetcher from '/@/use/fetcher'
 import PageContainer from '../components/Layout/PageContainer.vue'
-import GroupNameList from '../components/index/GroupNameList.vue'
+import GroupNameList from '../components/Index/GroupNameList.vue'
+import RecentList from '../components/Index/RecentList.vue'
 
 export default defineComponent({
   name: 'Index',
   components: {
     PageContainer,
-    GroupNameList
+    GroupNameList,
+    RecentList
   },
   setup() {
     const store = useStore()
@@ -25,10 +37,25 @@ export default defineComponent({
       store.dispatch.fetchGroups()
     )
 
+    const contests = computed(() => store.state.contests)
+    const { fetcherState: fetcherStateContests } = useFetcher(contests, () =>
+      store.dispatch.fetchContests()
+    )
+
     return {
       groups,
-      fetcherStateGroups
+      fetcherStateGroups,
+      contests,
+      fetcherStateContests
     }
   }
 })
 </script>
+
+<style lang="scss" module>
+.container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(330px, 1fr));
+  gap: 1rem;
+}
+</style>
