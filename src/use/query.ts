89d1497eeeb588bb60dc5ffbdd8129aaf7e7213d
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-10-17 23:31:14 +0900

    toStringIfArrayの返り値がanyだったので修正

--- src/use/query.ts
+++ src/use/query.ts
@@ -2,15 +2,16 @@ import { useRoute, LocationQueryValue } from 'vue-router'
 import { computed, ComputedRef } from 'vue'
 
 const toStringIfArray = (
-  s: LocationQueryValue | undefined | readonly LocationQueryValue[]
+  s: LocationQueryValue | undefined | LocationQueryValue[]
 ) => (Array.isArray(s) ? s[0] : s)
 
 /**
- * @param queryName - 取得するquery、存在しない場合 undefined を返す
+ * @param queryName - 取得するquery
+ * @returns 存在しない場合はundefind
  */
-const useQuery = (queryName: string): ComputedRef<string> => {
+const useQuery = (queryName: string): ComputedRef<string | undefined> => {
   const route = useRoute()
-  return computed(() => toStringIfArray(route.query[queryName]))
+  return computed(() => toStringIfArray(route.query[queryName]) ?? undefined)
 }
 
 export default useQuery
