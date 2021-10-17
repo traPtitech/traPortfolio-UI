303e5b2d036b8e24c7e504a84b0c551abf1a2985
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-10-17 20:20:12 +0900

    no non null assertionを消す

--- src/use/query.ts
+++ src/use/query.ts
@@ -6,12 +6,11 @@ const toStringIfArray = (
 ) => (Array.isArray(s) ? s[0] : s)
 
 /**
- * @param queryName - 取得するquery、存在しないものを取得しようとした場合、nullが返るので注意
+ * @param queryName - 取得するquery、存在しない場合 undefined を返す
  */
-const useQuery = (queryName: string): ComputedRef<string> => {
+const useQuery = (queryName: string): ComputedRef<string | undefined> => {
   const route = useRoute()
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  return computed(() => toStringIfArray(route.query[queryName])!)
+  return computed(() => toStringIfArray(route.query[queryName]))
 }
 
 export default useQuery
