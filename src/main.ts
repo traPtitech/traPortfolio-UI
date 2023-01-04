c5a57c66d158ff4e570c0bdc66ce50831dbbf5e6
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2023-01-04 16:35:50 +0900

    ⚡ piniaの導入

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import router from './router'
 import store from './store'
 import App from './App.vue'
@@ -6,7 +7,9 @@ import App from './App.vue'
 import './index.scss'
 import '@purge-icons/generated'
 
+const pinia = createPinia()
 const app = createApp(App)
 app.use(router)
 app.use(store)
+app.use(pinia)
 app.mount('#app')
