de0108fe14abd0b5a34db8f8d15b9dcdb3321b55
Pugma <maronmarondelicious@gmail.com>
2024-06-22 07:25:20 +0900

    `/user`以下のパスがtraQUIDになったのに合わせてリンクも修正

--- src/components/ContestTeam/MemberListItem.vue
+++ src/components/ContestTeam/MemberListItem.vue
@@ -9,7 +9,7 @@ defineProps<{
 
 <template>
   <li>
-    <router-link :class="$style.link" :to="`/users/${member.id}`">
+    <router-link :class="$style.link" :to="`/users/${member.name}`">
       <user-icon :class="$style.icon" :user-id="member.name" :size="32" />
       <p :class="$style.name">{{ member.name }}</p>
     </router-link>
