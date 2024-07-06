0acb7256eb6c48395ef32b207a0efbd64acfcfda
Pugma <maronmarondelicious@gmail.com>
2024-07-06 13:25:39 +0900

    アイコンを使う側でコンポーネントを選べるように

--- src/components/User/AccountListItem.vue
+++ src/components/User/AccountListItem.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import AIcon from '/@/components/UI/AIcon.vue'
+import AtCoder from '/@/assets/AtCoder.png'
 import { Account } from '/@/lib/apis'
 import { services } from '/@/consts/services'
 
@@ -18,7 +19,12 @@ defineProps<Props>()
       target="_blank"
       rel="noreferrer noopener"
     >
-      <a-icon :name="services.get(account.type)?.icon ?? ''" :size="24" />
+      <a-icon
+        v-if="services.get(account.type)?.icon !== 'atcoder'"
+        :name="services.get(account.type)?.icon ?? ''"
+        :size="24"
+      />
+      <img v-else :src="AtCoder" width="24px" height="24px" />
     </a>
   </li>
 </template>
