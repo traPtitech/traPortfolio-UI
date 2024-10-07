f4f7aef249649b57d29076694f41d5c04017c9fe
Futadaruma <0098yuta@gmail.com>
2024-10-07 13:27:48 +0900

    作業途中

--- src/components/Search/UserList.vue
+++ src/components/Search/UserList.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
 import UserListItem from './UserListItem.vue'
 import apis from '/@/lib/apis'
+import { ref, computed } from 'vue'
+import { searchListCaseInsensitive } from '/@/lib/search'
 
 interface Props {
   query: string
@@ -8,16 +10,28 @@ interface Props {
 const props = defineProps<Props>()
 
 const users = (await apis.getUsers(undefined, props.query)).data
+
+
+const filtered = computed(() =>
+  searchListCaseInsensitive(props.users, search.value, 'name')
+)
+const users1 = ref<User[]>(contestTeam.members)
+
+
 </script>
 
 <template>
   <div :class="$style.container">
     <ul :class="$style.list">
+
+
       <user-list-item
-        v-for="member in users"
+        v-for="member in users1"
         :key="member.id"
         :member="member"
       />
+
+
     </ul>
   </div>
 </template>
