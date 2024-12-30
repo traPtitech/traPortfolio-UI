2ebd26c6f7e239f16f856e8c28d1c93e2b4932d6
Pugma <maronmarondelicious@gmail.com>
2024-12-30 23:26:33 +0900

    fix typo

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,11 +1,11 @@
 import { handlers as userHandlers } from './users'
 import { handlers as contestHandlers } from './contests'
-import { handlers as groupeHandlers } from './groups'
+import { handlers as groupHandlers } from './groups'
 import { handlers as projectHandlers } from './projects'
 
 export const handlers = [
   ...userHandlers,
   ...contestHandlers,
-  ...groupeHandlers,
+  ...groupHandlers,
   ...projectHandlers
 ]
