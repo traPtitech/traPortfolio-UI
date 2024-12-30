14b5bdb5d772320928e61dde11519bd87ede63d2
Pugma <maronmarondelicious@gmail.com>
2024-12-31 00:59:54 +0900

    enabled `Top-level await`

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
-
+import { initMock } from './mocks/handler'
 import './index.scss'
 
-import { setupWorker } from 'msw/browser'
-
-if (import.meta.env.DEV) {
-  const { handlers } = await import('./mocks/handler')
-  const server = setupWorker(...handlers)
-  await server.start({ onUnhandledRequest: 'bypass' })
-}
+await initMock()
 
 const pinia = createPinia()
 const app = createApp(App)
--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -2,10 +2,18 @@ import { handlers as userHandlers } from './users'
 import { handlers as contestHandlers } from './contests'
 import { handlers as groupHandlers } from './groups'
 import { handlers as projectHandlers } from './projects'
+import { setupWorker } from 'msw/browser'
 
-export const handlers = [
+const handlers = [
   ...userHandlers,
   ...contestHandlers,
   ...groupHandlers,
   ...projectHandlers
 ]
+
+export const initMock = async () => {
+  if (import.meta.env.DEV) {
+    const server = setupWorker(...handlers)
+    await server.start({ onUnhandledRequest: 'bypass' })
+  }
+}
