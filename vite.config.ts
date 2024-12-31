4ba7137c71f4387d48fc0a422efc4bf6e6b188eb
Pugma <maronmarondelicious@gmail.com>
2024-12-31 18:16:54 +0900

    revert: build target

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import VuePlugin from '@vitejs/plugin-vue'
 const srcPath = path.resolve(__dirname, 'src').replace(/\\/g, '/')
 
 const config: UserConfig = {
-  build: {
-    target: ['esnext', 'edge89', 'firefox89', 'chrome89', 'safari15']
-  },
   resolve: {
     alias: {
       '/@': srcPath
