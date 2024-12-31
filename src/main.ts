388e1977f2d638fc15f66ffdaa99c4ff2c5fe9a6
Pugma <maronmarondelicious@gmail.com>
2024-12-31 18:14:38 +0900

    followed the way of enabling mock on reference

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,25 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
-import { initMock } from './mocks/handler'
 import './index.scss'
 
-await initMock()
+const enableMocking = async () => {
+  if (!import.meta.env.DEV) {
+    return
+  }
 
-const pinia = createPinia()
-const app = createApp(App)
-app.use(router)
-app.use(pinia)
-app.mount('#app')
+  const { worker } = await import('./mocks/handler')
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start({ onUnhandledRequest: 'bypass' })
+}
+
+enableMocking().then(() => {
+  const pinia = createPinia()
+  const app = createApp(App)
+
+  app.use(router)
+  app.use(pinia)
+  app.mount('#app')
+})
